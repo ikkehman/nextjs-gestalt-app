03572c55a6bfd710945cfed9d5a23d0d2ef0f9ca
Raimuko Yoasu <62538390+ikkehman@users.noreply.github.com>
2025-07-19 20:16:49 +0700

    Add .next/static/chunks/src_app_login_page_tsx_a993527f._.js 1752931008922

---
+++ .next/static/chunks/src_app_login_page_tsx_a993527f._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/node_modules_bcb62c1c._.js",
+  "static/chunks/src_26bbe7a2._.js"
+],
+    source: "dynamic"
+});
